9daf406513bb1558b3593c40d67c1642a789b48a
sarame <saramelad94@gmail.com>
2021-01-19 00:54:08 +0200

    RealTimeCharts Project

---
+++ README.md
@@ -0,0 +1,2 @@
+# SignalR_RealTimeChart with .NET Core and Angular
+This repository contains source code of ".NET Core & Angular" project
---
+++ RealTimeChartsClient/src/app/services/signal-r.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import * as signalR from "@aspnet/signalr";
+import { ChartModel } from '../interfaces/chart.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignalRService {
+
+  public data: ChartModel[];
+  public bradcastedData: ChartModel[];
+private hubConnection: signalR.HubConnection
+  public startConnection = () => {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+                            .withUrl('https://localhost:5001/chart')
+                            .build();
+    this.hubConnection
+      .start()
+      .then(() => console.log('Connection started'))
+      .catch(err => console.log('Error while starting connection: ' + err))
+  }
+  public addTransferChartDataListener = () => {
+    this.hubConnection.on('transferchartdata', (data) => {
+      this.data = data;
+      console.log(data);
+    });
+  }
+  public broadcastChartData = () => {
+    const data = this.data.map(m => {
+      const temp = {
+        data: m.data,
+        label: m.label
+      }
+      return temp;
+    });
+    this.hubConnection.invoke('broadcastchartdata', data)
+    .catch(err => console.error(err));
+  }
+  public addBroadcastChartDataListener = () => {
+    this.hubConnection.on('broadcastchartdata', (data) => {
+      this.bradcastedData = data;
+    })
+  }
+}
